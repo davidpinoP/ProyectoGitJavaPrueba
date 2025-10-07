2710cbf3415d49c404ee8f35bdc48cca5fa8560d
davidpinoP <pino7david@gmail.com>
2025-10-07 17:56:13 +0200

    “Añadida función saludar() en la rama saludo

--- ProyectoGitJava/src/Project/mainGitEjercicio.java
+++ ProyectoGitJava/src/Project/mainGitEjercicio.java
@@ -5,7 +5,10 @@ public class mainGitEjercicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola desde la rama main!");
+		saludar();
 	}
 
+	 public static void saludar() {
+	        System.out.println("Hola, soy David!");
+	    }
 }
